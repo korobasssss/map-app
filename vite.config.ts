9453aa561a1b20a4d3858f6143c7f731ea23baea
korobasssss <an.korobeynikova@gmail.com>
2024-10-10 00:49:39 +0300

    update configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {
+      'API_KEY': '614f0dc4-4b7c-4b09-96df-300aca71f0d0',
+      'SERVER_URL': 'http://localhost:3000'
+    }
+  }
 })
