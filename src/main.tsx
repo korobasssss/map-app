13ee5a6bafd762fad61ebee1f399179306675336
korobasssss <an.korobeynikova@gmail.com>
2024-10-10 00:52:19 +0300

    changed main files

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './store/rootStore.ts'
+
+const main_store = store()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={main_store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
